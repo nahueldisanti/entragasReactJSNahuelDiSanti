890df2dd0c0860edfdfd99c540cbecdd80093ece
nahuel di santi <nahuel.disanti@gmail.com>
2022-08-23 00:56:59 -0300

    botton agregado

--- src/Componentes/CartWidget.jsx
+++ src/Componentes/CartWidget.jsx
@@ -5,6 +5,7 @@ const CartWidget = () => {
     return (
         <div>
             <button type="button" className="btn btn-secondary" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="bottom" data-bs-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">
+            <i className="fa-solid fa-cart-shopping"></i>
             </button>
         </div>
     );
