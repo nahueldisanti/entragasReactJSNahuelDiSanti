eadd250d6b7260b495d3bf36d612696c0c6c9d10
nahuel di santi <nahuel.disanti@gmail.com>
2022-08-22 23:34:30 -0300

    Con App.jsx

---
+++ src/Components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import NavBar from './NavBar';
+
+const App = () => {
+    return (
+        <div>
+            <h1>ALTA LLAMA</h1>
+            <NavBar/> 
+        </div>
+    );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import NavBar from './Components/NavBar';
+import App from './Components/App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <NavBar />
+    <App />
   </React.StrictMode>
 );
 
