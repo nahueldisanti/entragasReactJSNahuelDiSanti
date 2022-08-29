52d67ee458dceedb1a7a417e188c3d6c33ffeb9a
nahuel di santi <nahuel.disanti@gmail.com>
2022-08-29 20:24:26 -0300

    Contador agregado

--- src/Componentes/Contador.jsx
+++ src/Componentes/Contador.jsx
@@ -1,10 +1,41 @@
-import React from 'react';
+import {useState} from 'react';
 
-const Contador = () => {
+const Contador = ({stock, onAdd}) => {
+        const [count, setCount] = useState(1);
+
+        function sumar(){
+            if (count < stock){
+            setCount(count + 1);}
+        }
+
+        function restar(){
+            if (count > 0){
+            setCount(count - 1);
+            }}
+
+        function resetear(){
+            setCount(0);
+        }
+
     return (
-        <div className = "counter-box">
-            <h1>Contador</h1>
-            <p>Stock:</p>
+        <div className="card border-secondary mb-3" style={{maxWidth: '20rem'}}>
+            <div className="card-header">Aniadir al carrito</div>
+            <div className="card-body">
+                <div className = "counter-box">
+                    <p>Stock: {stock}</p>
+                    <p>Cantidad: {count}</p>
+                </div>
+
+                <div>
+                    <button className="btn btn-primary" onClick ={restar}> - </button>
+                    <button className="btn btn-success" onClick ={sumar}>+</button>
+                    <button className="btn btn-info" onClick ={resetear}>Resetear</button>
+                </div>
+
+                <div>
+                    <button className="btn btn-warning" onClick={() => onAdd(count)}>Aniadir al carrito</button>
+                </div>
+            </div>
         </div>
     );
 }
--- src/Componentes/ItemListContainer.jsx
+++ src/Componentes/ItemListContainer.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './ItemListContainer.css'
+import Contador from './Contador.jsx'
 
 const ItemListContainer = ({titulo}) => {
+
+    function onAdd(count){
+        console.log(`Han sido aniadidos ${count} elementos al carrito`);
+        alert(`Han sido aniadidos ${count} elementos al carrito`);
+    } 
     return (
         <div>
             <h1>{titulo}</h1>
+            <Contador stock ={10} onAdd={onAdd} />
         </div>
     );
 }
