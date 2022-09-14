9c35799d4f59caf7671c49606739b720488a411f
nahuel di santi <nahuel.disanti@gmail.com>
2022-09-14 02:01:00 -0300

    solucionado un warninf

--- src/Componentes/ProductosList.jsx
+++ src/Componentes/ProductosList.jsx
@@ -4,7 +4,7 @@ import Productos from './Productos';
 const ProductosList = ({list}) => {
     return (
         <ul>
-            {list.map( (producto) => <li><Productos producto={producto}/></li>)}
+            {list.map( (producto) => <li key={producto.id}><Productos producto={producto}/></li>)}
         </ul>
     );
 }
